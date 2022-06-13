31f56db2ca95d91b20bf6748a1bceb93e8e7ffe5
Aleksey <Alekseydemochkin93@gmail.com>
2022-06-13 12:28:10 +0300

    исправил задачу 2

--- S2_L2_Task2/Program.cs
+++ S2_L2_Task2/Program.cs
@@ -7,28 +7,19 @@ string param2 = Console.ReadLine();
 int num2 = int.Parse(param2);
 string param3 = Console.ReadLine();
 int num3 = int.Parse(param3);
-int max = 0;
+int max = num1;
 
 if (num1 == num2 && num2 == num3)
 {
     System.Console.WriteLine($"числа равны");
     Environment.Exit(0);
 }
-if (num1>num2)
+if (max < num2);
 {
- max = num1;
- if (num1>num3)
- {
-     System.Console.WriteLine($"макс первое {max}" );
- }  
+    max = num2;
 }
-else if (num3>num1)                 //хотел запихнуть это внутрь предидущего if (строка 18), но почему то ругался чувствую что можно записать короче
+if ( max < num3);
 {
     max = num3;
-    System.Console.WriteLine($"макс третье {max}");
 }
-else
- {
-    max = num2;
-     System.Console.WriteLine($"макс второе {max}");
- }
+System.Console.WriteLine($"максимальное число {max}");

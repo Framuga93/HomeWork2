aad04a4997d1593b441e5a0d433013d78caf5acb
Framuga93 <alekseydemochkin93@gmail.com>
2022-07-04 19:38:37 +0300

    add HW 6

---
+++ lesson6/task1/Program.cs
@@ -0,0 +1,50 @@
+﻿//Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string number = Console.ReadLine();
+    int numInt = int.Parse(number);
+    return numInt;
+}
+
+int[] GetNumber(int number)
+{
+    int[] numberArray = new int[number];
+    for (int i = 0; i < number; i++)
+    {
+        numberArray[i] = Prompt($"введите {i + 1} число ");
+    }
+    return numberArray;
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for (int position = 0; position < count; position++)
+    {
+        Console.Write($"{col[position]} ");
+    }
+}
+
+int GetPositiveNumbers(int[] array)
+{
+    int countPositive = 0;
+    foreach (int elemenet in array)
+    {
+        if (elemenet>0)
+        {
+            countPositive++;
+        }
+    }
+    return countPositive;
+}
+
+
+
+int number = Prompt("сколько чисел вы хотите ввести? ");
+int[] numberArray = GetNumber(number);
+int countPositive = GetPositiveNumbers(numberArray);
+System.Console.WriteLine($"вы ввели {countPositive} чисел больше 0");
---
+++ lesson6/task2/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 2. Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string number = Console.ReadLine();
+    int numInt = int.Parse(number);
+    return numInt;
+}
+
+
+
+
+
+double b1 = Prompt("введите значение b1 ");
+double k1 = Prompt("введите значение k1 ");
+double b2 = Prompt("введите значение b2 ");
+double k2 = Prompt("введите значение k2 ");
+
+
+if (k1==k2 || b1==b2)
+{
+    System.Console.WriteLine("точки пересечения нет");
+}
+
+double x = (b1-b2)/(k1-k2);
+double y = k1*x+b1;
+System.Console.WriteLine($"координаты точек пересечения {x} , {y}");
+
+
+
